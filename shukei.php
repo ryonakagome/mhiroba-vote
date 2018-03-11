1485c630384fc3221041a980385ee2dd9bd1e363
Ryo Nakagome <nakagome@tento-net.com>
2018-03-11 14:50:59 +0900

    集計ページを作成

---
+++ shukei.php
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<html charset="UTF-8" lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="集計ページ" />
