ff7ac0b7abeca3331c38ce1f116f5162237b293e
Ryo Nakagome <nakagome@tento-net.com>
2018-03-11 16:16:23 +0900

    集計結果ページのファイルを作成

--- shukei.php
+++ shukei.php
@@ -4,3 +4,14 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="集計ページ" />
+        
+        <title>集計結果 - 超まいくらひろば2018</title>
+
+        <link rel="stylesheet" href="shukei.css">
+    </head>
+    <body>
+        <div id="shukei">
+
+        </div>
+    </body>
+</html>
