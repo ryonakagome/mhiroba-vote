e9dfa1a480fb04648f32ab8a354f7133eba1492d
Ryo Nakagome <nakagome@tento-net.com>
2018-04-22 20:50:50 +0900

    投票プログラムを修正

--- votedo.php
+++ votedo.php
@@ -6,6 +6,7 @@
         $id = $_GET['id'];
         require_once('config/config.php');
         $sql = mysqli_query($db_link, "INSERT INTO mvote_vote VALUES ('$id')");
+        $sql = mysqli_query($db_link, "UPDATE mvote_kabegami SET Voting = Voting + 1 WHERE KabegamiID = '$id'");
         print('<script>location.href = "home.php";</script>');
     }
 ?>
