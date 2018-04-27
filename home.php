dd8666f1b42ad2038ab34af627554be16ee3dac0
yuko fuyutsuki <yuko_fuyutsuki@pmmp.tokyo>
2018-04-27 21:41:54 +0900

    考えてはいけない。俺たちは感覚でコードを書いている。

--- home.php
+++ home.php
@@ -24,14 +24,14 @@
         <a href="shukei.php">集計画面を見る</a>
         <a href="shukei-textonly.php">簡易集計画面を見る</a>
         <h2>投票</h2>
-        <table border="1">
+        <table border="1" class="table-width">
             <?php
                 require_once('config/config.php');
                 $sql = mysqli_query($db_link, "SELECT * FROM mvote_kabegami");
                 $number = 0;
                 print("<tr>");
                 while ($result = mysqli_fetch_assoc($sql)) {
-                    print('<td><a href="votedo.php?id='.$result['KabegamiID'].'"><font size="10"><b>'.$result['KabegamiID'].'</b></font></a></td>');
+                    print('<td class="cell-button"><a href="votedo.php?id='.$result['KabegamiID'].'"><font size="10"><b>'.$result['KabegamiID'].'</b></font></a></td>');
                     $number = $number + 1;
                     if($number == 5) {
                         print("</tr><tr>");
@@ -42,7 +42,7 @@
         </table>
         <div id="hanbun">
             <br>
-            <table>
+            <table class="table-width">
                 <tr><th>作品名</th><th>票数</th></tr>
                 <?php
                     require_once('config/config.php');
